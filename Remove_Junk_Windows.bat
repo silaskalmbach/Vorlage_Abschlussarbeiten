33575229af12689aab8472f8e379fa225e221517
silaskalmbach <silas.kalmbach@gmail.com>
2022-07-18 16:32:23 +0200

    Erweiterungen von Flags und Umstrukturierung

--- Remove_Junk_Windows.bat
+++
@@ -1,1 +0,0 @@
-del *.aux *.log *.lof *.gz *.toc *.bak~ *.bbl **.bcf *.blg *.lot *.out *.xml *.acn *.acr *.alg *.glg *.glo *.gls *.gug *.guo *.gus *.ist *.llg *.llo *.lls *.lug *.luo *.lus *.mog *.moo *.mos *.xdy *.mw
---
+++ _E_ressources/Remove_Junk_Windows.bat
@@ -0,0 +1,4 @@
+cd ..
+del *.aux *.log *.lof *.gz *.toc *.bak~ *.bbl **.bcf *.blg *.lot *.out *.xml *.acn *.acr *.alg *.glg *.glo *.gls *.gug *.guo *.gus *.ist *.llg *.llo *.lls *.lug *.luo *.lus *.mog *.moo *.mos *.xdy *.mw
+cd ./_A_frontmatter/01_frontcover
+del *.aux *.log *.lof *.gz *.toc *.bak~ *.bbl **.bcf *.blg *.lot *.out *.xml *.acn *.acr *.alg *.glg *.glo *.gls *.gug *.guo *.gus *.ist *.llg *.llo *.lls *.lug *.luo *.lus *.mog *.moo *.mos *.xdy *.mw
